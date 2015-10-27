400cd5e804c7d5cc60f97aa1bc9625a34321956c
rnirushan <nirushan.11@cse.mrt.ac.lk>
2015-10-27 21:10:37 +0530

    set landing page screen height

--- app/cache/dev/twig/9e/06/922de827685e23bd23a3dcc2f2bb2b3b15b194453d345b34384a080dfe68.php
+++ app/cache/dev/twig/9e/06/922de827685e23bd23a3dcc2f2bb2b3b15b194453d345b34384a080dfe68.php
@@ -22,45 +22,69 @@ class __TwigTemplate_9e06922de827685e23bd23a3dcc2f2bb2b3b15b194453d345b34384a080
         // line 3
         $this->env->loadTemplate("LoginLoginBundle:Default:head_v2.html.twig")->display($context);
         echo "\t
-<body>
-
-    <div id=\"wrapper\">
+<body id=\"page-top\" data-spy=\"scroll\" data-target=\".navbar-fixed-top\">
 
         <!-- Navigation -->
-        ";
-        // line 9
-        $this->env->loadTemplate("LoginLoginBundle:Default:navigation.html.twig")->display($context);
-        // line 10
-        echo "\t\t<!-- Second Menu -->\t\t
-\t\t<!-- <?php include(\"secondmenu.php\"); ?> -->
-
-     <!-- Page Content -->\t\t
-\t<div id=\"page-wrapper\">
-\t\t<div class=\"container-fluid\">
-            <div class=\"row\">
-                <div class=\"col-lg-12\">
-
-                </div>
-                <!-- /.col-lg-12 -->
-            </div>
-            <!-- /.row -->
-
-            
-                </div>
-                    
-        </div>
-        <!-- /#page-wrapper -->
 \t";
-        // line 29
+        // line 7
+        $this->env->loadTemplate("LoginLoginBundle:Default:navigation.html.twig")->display($context);
+        // line 8
+        echo "\t\t<!-- Second Menu -->\t\t
+\t";
+        // line 9
+        $this->env->loadTemplate("LoginLoginBundle:Default:secondmenu.html.twig")->display($context);
+        // line 10
+        echo "        <section id=\"intro\" class=\"intro-section\">
+            <div class=\"container-fluid\">
+                <div class=\"row\">
+                    <div class=\"col-md-4\">
+                    </div>
+                    <div class=\"col-md-4\">
+                    </div>
+                    <div class=\"col-md-4\">
+                    </div>
+\t\t<!-- /.col-lg-12 -->
+\t\t</div>
+                <!-- /.row -->
+\t\t<div class=\"row\">
+                    <div class=\"col-md-4\">
+                    </div>
+                    <div class=\"col-md-4\">
+                        
+                    </div>
+                    <div class=\"col-md-4\">
+                    </div>
+                    <!-- /.col-lg-4 -->
+                </div>
+                <!-- /.row -->
+            </div>
+            <!-- /.container-fluid -->
+        </section>
+                    
+        <div class=\"container-fluid hidden-lg hidden-md\">
+            <div class=\"row\">
+                    <div class=\"col-lg-12\" style=\"height: 200px;z-index: -1;\">
+                        
+                    </div>
+            </div>
+        </div>
+                    
+        <section id=\"about\" class=\"about-section\">
+            <div class=\"container-fluid\">
+                <div class=\"row\">
+                    <div class=\"col-lg-12\" style=\"padding-left: 0;padding-right: 0\">
+                        ";
+        // line 49
         $this->env->loadTemplate("LoginLoginBundle:Default:footer_v2.html.twig")->display($context);
-        // line 30
-        echo "
-    </div>
-    <!-- /#wrapper -->
+        // line 50
+        echo "                    </div>
+                </div>
+            </div>
+        </section>
         ";
-        // line 33
+        // line 54
         $this->env->loadTemplate("LoginLoginBundle:Default:scripts.html.twig")->display($context);
-        // line 34
+        // line 55
         echo "
 </body>
 
@@ -80,6 +104,6 @@ class __TwigTemplate_9e06922de827685e23bd23a3dcc2f2bb2b3b15b194453d345b34384a080
 
     public function getDebugInfo()
     {
-        return array (  64 => 34,  62 => 33,  57 => 30,  55 => 29,  34 => 10,  32 => 9,  23 => 3,  19 => 1,);
+        return array (  88 => 55,  86 => 54,  80 => 50,  78 => 49,  37 => 10,  35 => 9,  32 => 8,  30 => 7,  23 => 3,  19 => 1,);
     }
 }
