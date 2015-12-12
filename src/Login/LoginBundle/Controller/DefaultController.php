0583989be435ac557bbb2d47d2e3d540eeea3543
Sirojan Tharmakulasingam <sirojan.11@cse.mrt.ac.lk>
2015-12-12 16:26:24 +0530

    sharing backend added

--- src/Login/LoginBundle/Controller/DefaultController.php
+++ src/Login/LoginBundle/Controller/DefaultController.php
@@ -7,20 +7,18 @@ use Symfony\Component\HttpFoundation\Request;
 use Login\LoginBundle\Entity\Users;
 use Symfony\Component\HttpFoundation\Response;
 use Login\LoginBundle\Modals\Login;
-class DefaultController extends Controller
-{
-    public function homeAction(Request $request)
-    {
+
+class DefaultController extends Controller {
+
+    public function homeAction(Request $request) {
         return $this->render('LoginLoginBundle:Default:landing.html.twig');
     }
-    
-    public function signinAction(Request $request)
-    {
+
+    public function signinAction(Request $request) {
         return $this->render('LoginLoginBundle:Default:signinV2.html.twig');
     }
-    
-    public function indexAction(Request $request)
-    {
+
+    public function indexAction(Request $request) {
         if ($request->getMethod() == 'POST') {
 
             $username = $request->get('username');
@@ -31,8 +29,8 @@ class DefaultController extends Controller
             $user = $repository->findOneBy(array('username' => $username, 'password' => $password));
 
             if ($user) {
-                if($user->getStatus() == 'Active'){
-                    $fullname = $user->getFirstname()." ".$user->getLastname();
+                if ($user->getStatus() == 'Active') {
+                    $fullname = $user->getFirstname() . " " . $user->getLastname();
                     $session = $this->getRequest()->getSession();
                     $session->clear();
                     $login = new Login();
@@ -44,32 +42,28 @@ class DefaultController extends Controller
 
 
 
-                    return $this->render('LoginLoginBundle:Default:dashboard.html.twig', array('name' => $user->getUsername(),'role' => $user->getRole(),'fullname'=>$fullname, 'manageview'=>$user->getUserView()));
-                }else{
+                    return $this->render('LoginLoginBundle:Default:dashboard.html.twig', array('name' => $user->getUsername(), 'role' => $user->getRole(), 'fullname' => $fullname, 'manageview' => $user->getUserView()));
+                } else {
                     return $this->render('LoginLoginBundle:Default:signinV2.html.twig', array('errormsg' => 'Oh snap! You are not an active user'));
-		
                 }
-            }else{
+            } else {
 
-		return $this->render('LoginLoginBundle:Default:signinV2.html.twig', array('errormsg' => 'Oh snap! Login Failed Try Again'));
-			
+                return $this->render('LoginLoginBundle:Default:signinV2.html.twig', array('errormsg' => 'Oh snap! Login Failed Try Again'));
             }
-        }else{
+        } else {
 
-			return $this->render('LoginLoginBundle:Default:signinV2.html.twig');
-			
-		} 
+            return $this->render('LoginLoginBundle:Default:signinV2.html.twig');
+        }
     }
-    
-    public function forgetPasswordAction(Request $request)
-    {
+
+    public function forgetPasswordAction(Request $request) {
         return $this->render('LoginLoginBundle:Default:forgetPassword.html.twig');
     }
-    
+
     public function signupAction(Request $request) {
-        
-        if($request->getMethod()=='POST'){
-            
+
+        if ($request->getMethod() == 'POST') {
+
             $username = $request->get('username');
             $companyname = $request->get('companyname');
             $email = $request->get('email');
@@ -78,26 +72,24 @@ class DefaultController extends Controller
             $industry = $request->get('industry');
             $firstname = $request->get('firstname');
             $lastname = $request->get('lastname');
-            
+
             $em = $this->getDoctrine()->getManager();
             $repository = $em->getRepository("LoginLoginBundle:Users");
 
             $firstuser = $repository->findOneBy(array('companyname' => $companyname));
-            
-            if($firstuser){
-                
-                 $role = "Regular";
-                
-            }else{
-                
+
+            if ($firstuser) {
+
+                $role = "Regular";
+            } else {
+
                 $role = "Admin";
-                
             }
-            
-           
+
+
             $status = "Active";
             $user = new Users();
-            
+
             $user->setCompanyname($companyname);
             $user->setUsername($username);
             $user->setEmail($email);
@@ -112,125 +104,116 @@ class DefaultController extends Controller
             $user->setOpportunityview(10);
             $user->setContactview(10);
             //$em = $this->getDoctrine()->getEntityManager();
-            try{
+            try {
                 $em->persist($user);
                 $em->flush();
                 return $this->render('LoginLoginBundle:Default:signinV2.html.twig', array('successmsg' => 'Registration Success,Login your account.'));
-            } catch(Doctrine\ORM\ORMInvalidArgumentException $e){
-                
+            } catch (Doctrine\ORM\ORMInvalidArgumentException $e) {
+
                 return $this->render('LoginLoginBundle:Default:signIn.html.twig', array('errormsg' => 'Invalid Arguments. Try Again'));
-                
             }
-           
         }
         return $this->render('LoginLoginBundle:Default:signIn.html.twig');
     }
-    
+
     public function signupV2Action(Request $request) {
         return $this->render('LoginLoginBundle:Default:signup.html.twig');
     }
-    
-    public function uservalidateajaxAction(Request $request){
-        
-         $username = $request->get('username');
-         
-             
-         $em = $this->getDoctrine()->getManager();
-         $repository = $em->getRepository("LoginLoginBundle:Users");
-            
-         $user = $repository->findOneBy(array('username' => $username));
-         
-         if($user){  
-            return new Response("true"); 
-         }else{
-            return new Response("false");
-             
-         }
-             
-         
-        
-        
-    }
-    
-    public function getusersAction (Request $request){
-        
-        $sharedstring = $request->get('sharedusers');
-        $originator = $request->get('user');
-        $sharedusers = split(":", $sharedstring);
+
+    public function uservalidateajaxAction(Request $request) {
+
+        $username = $request->get('username');
+
+
         $em = $this->getDoctrine()->getManager();
         $repository = $em->getRepository("LoginLoginBundle:Users");
-        $commisionstring = '';
-        foreach ($sharedusers as $shareduser) {
-           $user = $repository->findOneBy(array('username' => $shareduser)); 
-           $commisionstring .= $user->getFirstname().' '.$user->getLastname().'*'.$user->getCommissionnonoriginator();
-           $commisionstring .= ':';
-           
+
+        $user = $repository->findOneBy(array('username' => $username));
+
+        if ($user) {
+            return new Response("true");
+        } else {
+            return new Response("false");
         }
+    }
+
+    public function getusersAction(Request $request) {
+
+        $sharedstring = $request->get('sharedusers');
+        $originator = $request->get('user');
+        $em = $this->getDoctrine()->getManager();
+        $repository = $em->getRepository("LoginLoginBundle:Users");
+
         $originuser = $repository->findOneBy(array('username' => $originator));
-        $commisionstring .= $originuser->getFirstname().' '.$originuser->getLastname().'*'.$originuser->getCommissionoriginator();
+
+        $dataArray = array("originatorname" => $originuser->getFirstname() . ' ' . $originuser->getLastname(),
+            "orininatorpercentage" => $originuser->getCommissionoriginator());
+
+        if ($sharedstring != '') {
+            $sharingArray = array();
+            $sharedusers = split(":", $sharedstring);
+            foreach ($sharedusers as $shareduser) {
+                $user = $repository->findOneBy(array('username' => $shareduser));
+                $arrElement["fullname"] = $user->getFirstname() . ' ' . $user->getLastname();
+                $arrElement["percentage"] = $user->$user->getCommissionnonoriginator();
+                array_push($sharingArray, $arrElement);
+            }
+            $dataArray["sharing"] = $sharingArray;
+        }
         
-        if($commisionstring){
-           return new Response($commisionstring);  
-        }else{
+        if($dataArray){
+            return new Response(json_encode($dataArray));
+        } else {
             return new Response("false");
-             
-         }
-        
-    } 
-    
-    public function getCommissionoriginatorAction(Request $request){
-        
-         $username = $request->get('name');
-         
-             
-         $em = $this->getDoctrine()->getManager();
-         $repository = $em->getRepository("LoginLoginBundle:Users");
-            
-         $user = $repository->findOneBy(array('username' => $username));
-         
-         if($user){  
-            return new Response($user->getCommissionoriginator()); 
-         }else{
-            return new Response("false");
-             
-         }
-             
-         
-        
-        
+        }
     }
-    
-    public function getFullnameAction(Request $request){
+
+    public function getCommissionoriginatorAction(Request $request) {
+
         $username = $request->get('name');
-         
-             
-         $em = $this->getDoctrine()->getManager();
-         $repository = $em->getRepository("LoginLoginBundle:Users");
-            
-         $user = $repository->findOneBy(array('username' => $username));
-         
-         if($user){  
-            return new Response($user->getFirstname()." ".$user->getLastname()); 
-         }else{
+
+
+        $em = $this->getDoctrine()->getManager();
+        $repository = $em->getRepository("LoginLoginBundle:Users");
+
+        $user = $repository->findOneBy(array('username' => $username));
+
+        if ($user) {
+            return new Response($user->getCommissionoriginator());
+        } else {
             return new Response("false");
-             
-         }
+        }
     }
-    
-    public function logoutAction(Request $request){
-        
+
+    public function getFullnameAction(Request $request) {
+        $username = $request->get('name');
+
+
+        $em = $this->getDoctrine()->getManager();
+        $repository = $em->getRepository("LoginLoginBundle:Users");
+
+        $user = $repository->findOneBy(array('username' => $username));
+
+        if ($user) {
+            return new Response($user->getFirstname() . " " . $user->getLastname());
+        } else {
+            return new Response("false");
+        }
+    }
+
+    public function logoutAction(Request $request) {
+
         $session = $request->getSession();
         $session->clear();
         return $this->render('LoginLoginBundle:Default:signinV2.html.twig');
-        
     }
-    
-    public function termsofServiceAction(Request $request){
+
+    public function termsofServiceAction(Request $request) {
         return $this->render('LoginLoginBundle:Default:termsofService.html.twig');
     }
-    
-    public function privacyAction(Request $request){
-         return $this->render('LoginLoginBundle:Default:privacy.html.twig');
+
+    public function privacyAction(Request $request) {
+        return $this->render('LoginLoginBundle:Default:privacy.html.twig');
     }
 
 }
